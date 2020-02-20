e71dda475dbb5beb1ffbf918d8ed68657540c48d
Lisa Wagner <wagnerl@mskcc.org>
2020-02-20 11:35:34 -0500

    Lint changes

--- igo-tools.py
+++ igo-tools.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='./build')
+app = Flask(__name__, static_folder='./dist')
 
 # Serve React App
 @app.route('/', defaults={'path': ''})
